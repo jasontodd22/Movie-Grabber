272ba4756e12375a1094a13e6787228613808050
jasontodd22 <okjasontodd@gmail.com>
2024-08-24 20:44:38 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
 document.getElementById('dark-mode-toggle').addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
+
+// Additional dynamic features
+document.querySelectorAll('.movie').forEach(movie => {
+    movie.addEventListener('mouseover', () => {
+        movie.querySelector('.movie-image').style.opacity = '0.8';
+    });
+    movie.addEventListener('mouseout', () => {
+        movie.querySelector('.movie-image').style.opacity = '1';
+    });
+});
